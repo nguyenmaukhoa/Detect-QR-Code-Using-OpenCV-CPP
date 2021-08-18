40668bf906b518239f1c5c8d5eae798206533c53
Khoa Nguyen <nguyenmaukhoa@gmail.com>
2021-08-18 17:00:00 -0400

    Switch from jupyter files to cpp ones

---
+++ build/CMakeFiles/qr_code_detector.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qr_code_detector.dir/qr_code_detector.cpp.o"
+  "CMakeFiles/qr_code_detector.dir/qr_code_detector.cpp.o.d"
+  "qr_code_detector"
+  "qr_code_detector.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/qr_code_detector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ qr_code_detector.cpp
@@ -0,0 +1,80 @@
+// Include libraries
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <iostream>
+#include <string>
+
+using namespace std;
+using namespace cv;
+
+// Function to read the qr code
+void display(Mat frame){
+	// Matrix object to catch the bounding box of the QR Code
+	Mat bbox, rectifiedImage;
+
+	// Create QR Code Detector object
+	QRCodeDetector qrDecoder = QRCodeDetector();
+
+	// Extract string data from the QR Code using method detectAndDecode()
+	string qrCodeData = qrDecoder.detectAndDecode(frame, bbox, rectifiedImage);
+
+	// Check if a QR Code has been detected
+	if(qrCodeData.length()>0){
+		// Extract bounding box starting coordinate
+		int p1_x = (int)bbox.at<float>(0,0);
+		int p1_y = (int)bbox.at<float>(0,1);
+
+		// Add QR Code Value on each frame
+		putText(frame, qrCodeData, Point(p1_x,p1_y), 
+				FONT_HERSHEY_SIMPLEX, 1.0, 
+				Scalar(0,255,0), 5);
+	}
+
+	// Display the frames
+	imshow("Frame", frame);
+}
+
+int main(){
+	// Create a VideoCapture object, and read and existing video
+	VideoCapture cap("../video/input.mp4");
+
+	// Disable the code above, and enable the one below to use webcam
+	// VideoCapture cap(0);
+
+	// Check the video is loaded sucessfully
+	if(!cap.isOpened()){
+		cout << "Error opening the video..." << endl;
+		return -1;
+	}
+
+	// Matrix to hold each video frame
+	Mat frame;
+
+	// Display the video until ESC key is pressed
+	while(1){
+		// Copy the video frame-by-frame from cap to frame
+		cap >> frame;
+
+		// If the frame is empty, stop the dislay
+		if(frame.empty())
+			break;
+
+		// Display QR Code value on the frame
+		display(frame);
+
+		// Stop when ESC key is pressed
+		char c = (char)waitKey(25);
+		if(c == 27)
+			break;
+	}
+	// Release the cap object when done
+	cap.release();
+
+	// Close all the windows
+	destroyAllWindows();
+	return 0;
+}
